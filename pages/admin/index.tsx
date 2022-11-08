67d0759aa7f486d2f0248fa89a1029f784ce6eba
Rohan Veer <rohanveer1989@gmail.com>
2022-11-08 12:21:20 +0530

    Added Admin Pages

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,11 +1,39 @@
 import Authenticated from "../../components/common/Authenticated";
+//import { getAuthSession } from "../../lib/session";
 
-export default function Admin() {
+export default function Admin({ user }: any) {
   return (
     <Authenticated redirectTo="/error">
       <div>
-        ADMIN PAGES
+        <h4>{`Welcome ${user}`}</h4>
+        <h6>ADMIN PAGES</h6>
       </div>
     </Authenticated>
   )
 }
+
+export async function getStaticProps() {
+  return {
+    props: {
+      user: 'Rohan Veer'
+    },
+  }
+}
+
+/* export async function getServerSideProps(context: any) {
+  const authSession = getAuthSession(context);
+  if (!authSession) {
+    return {
+      redirect: {
+        destination: "/error",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      user: authSession.user,
+    },
+  };
+} */
